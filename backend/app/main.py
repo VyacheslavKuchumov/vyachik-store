2ceb2a9b1e0c57148c8cfe845d7c205abce7e174
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2025-12-01 12:20:04 +0500

    make a redirect to docs on root and add DB generation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,25 @@
 # app/main.py
 from fastapi import FastAPI
-# Импортируем наш новый модуль с роутером
+from contextlib import asynccontextmanager
 from app.routers import user
 from app.database import Base, engine
+from fastapi.responses import RedirectResponse
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
 
-app = FastAPI(title="My Architecture App")
+app = FastAPI(title="Vyachik's Store Backend", lifespan=lifespan)
 
 # Подключаем роутер к главному приложению.
 # Это похоже на подключение плагина.
 app.include_router(user.router)
 
-# Создаем все таблицы в базе данных при старте приложения
-@app.on_event("startup")
-async def on_startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
 @app.get("/")
 def root():
-    return {"message": "Приложение работает!"}
+    # Редирект на документацию по API
+    return RedirectResponse(url="/docs", status_code=307)
